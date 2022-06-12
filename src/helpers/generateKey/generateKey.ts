adb448acd7d0740701aba4e6d026a406d47ed4e2
lanceraeltaylor <lancerael@hotmail.com>
2022-06-12 18:24:23 +0100

    Simplified helper

--- src/helpers/generateKey/generateKey.ts
+++ src/helpers/generateKey/generateKey.ts
@@ -2,6 +2,6 @@ const LETTERS = 'abcdefghijklm0123456'
 
 export const generateKey = (length = 15): string => Array(length).fill('').map(() => {
   const jsRandom = Math.floor((Math.random() * 10) + 1)
-  const trulyRandom = +`${+new Date()}`[`${+new Date()}`.length-1]
+  const trulyRandom = +`${+new Date()}`[12]
   return LETTERS[jsRandom+trulyRandom]
 }).join('')
